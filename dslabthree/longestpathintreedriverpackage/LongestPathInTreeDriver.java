32a66bfa758d96342499c3ef090a275e239d61f2
Prashant <prash.click1@gmail.com>
2022-01-30 17:30:47 +0530

    dsa lab 3 commit

--- dslabthree/longestpathintreedriverpackage/LongestPathInTreeDriver.java
+++ dslabthree/longestpathintreedriverpackage/LongestPathInTreeDriver.java
@@ -4,69 +4,59 @@ import java.util.ArrayList;
 
 public class LongestPathInTreeDriver {
     //Node Data And Behaviour
-    static class Node
-    {
-        Node left;
-        Node right;
+    static class Node {
+        Node leftNode;
+        Node rightNode;
         int data;
     };
-
     //Method to create a new Binary Node
-    static Node newNode(int data)
-    {
+    static Node newNode(int data) {
         Node temp = new Node();
         temp.data = data;
-        temp.left = null;
-        temp.right = null;
+        temp.leftNode = null;
+        temp.rightNode = null;
         return temp;
     }
 
     // Method to get longest path
-    public static ArrayList<Integer> longestPath(Node root)
-    {
+    public static ArrayList<Integer> longestPath(Node root) {
         // checkpoint: If root is null then return empty
-        if(root == null)
-        {
+        if (root == null) {
             ArrayList<Integer> list = new ArrayList<>();
             return list;
         }
         // Recursively calling for root.right subtree
-        ArrayList<Integer> rightList = longestPath(root.right);
+        ArrayList<Integer> rightList = longestPath(root.rightNode);
         // Recursively calling for root.left subtree
-        ArrayList<Integer> leftList = longestPath(root.left);
+        ArrayList<Integer> leftList = longestPath(root.leftNode);
         //comparing the size for left and right traversals, and based on comparision, add new root data on larger subtree
-        if(rightList.size() < leftList.size())
-        {
+        if (rightList.size() < leftList.size()) {
             leftList.add(root.data);
-        }
-        else
-        {
+        } else {
             rightList.add(root.data);
         }
         // Return back list with bigger size, depending on left and right traversals, for appended root appropriately
-        return (leftList.size() > rightList.size() ? leftList :rightList);
+        return (leftList.size() > rightList.size() ? leftList : rightList);
     }
 
     // Main Driver Program
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         //creating tree from problem statement
         Node root = newNode(100);
-        root.left = newNode(20);
-        root.right = newNode(130);
-        root.left.left = newNode(10);
-        root.left.right = newNode(50);
-        root.right.left = newNode(110);
-        root.right.right = newNode(140);
-//        root.left.left.left = newNode(5);
-
+        root.leftNode = newNode(20);
+        root.rightNode = newNode(130);
+        root.leftNode.leftNode = newNode(10);
+        root.leftNode.rightNode = newNode(50);
+        root.rightNode.leftNode = newNode(110);
+        root.rightNode.rightNode = newNode(140);
+        root.leftNode.leftNode.leftNode = newNode(5);
+        //Result Array List That Will Process With Recursion
         ArrayList<Integer> resultList = longestPath(root);
-        int n = resultList.size();
+        int size = resultList.size();
         System.out.print("Longest path ");
-        System.out.print(resultList.get(n - 1));
+        System.out.print(resultList.get(size - 1));
         //Printing In Reverse Order So That Path Tracing Starts From Root Towards Leaf Node
-        for(int i = n - 2; i >= 0; i--)
-        {
+        for (int i = size - 2; i >= 0; i--) {
             System.out.print(" -> " + resultList.get(i));
         }
     }
